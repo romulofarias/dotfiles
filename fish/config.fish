08bb9b45b36b37f76b717c5228e2203af931571f
Rômulo Brito de Farias <40609710+romulofarias@users.noreply.github.com>
2019-07-18 15:23:33 -0300

    Add files via upload.

---
+++ fish/config.fish
@@ -0,0 +1,10 @@
+# Using theme bobthefish with theme_color_scheme gruvbox
+# omf install bobthefish
+# Config theme bobthefish
+set -g theme_color_scheme gruvbox
+set -g theme_display_date no
+#set -g theme_date_format "+%a, %d.%m.%y (%Y.%j) [%Hh%M]"
+set -g theme_display_cmd_duration no
+
+
+
