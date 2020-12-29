9524118e25530dbfef76ca39ab6279b56a854682
romulofarias <fariasbrito.romulo@gmail.com>
2020-12-29 13:39:24 -0300

    script pos-instalação fedora

---
+++ fedora-scripts/pinstall.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+# enable access to both the free and nonfree repositories
+dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm \
+	https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
